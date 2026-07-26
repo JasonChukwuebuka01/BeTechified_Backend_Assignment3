429457136f3214007d58283783ed4b78c646d510
jason <jasonchinonso20@gmail.com>
2026-07-26 01:49:00 +0100

    Feat: implemented monogoDb to routes

--- app.js
+++ app.js
@@ -4,21 +4,30 @@ const logger = require("./middlewares/logger.middleware");
 const {
   validatePost,
   validatePatch,
+  validateId,
+  validateQuery,
 } = require("./middlewares/validatePost.middleware");
 const globalErrorHandler = require("./middlewares/globalError.middleware");
+const connectDB = require("./config/db");
+const todoModel = require("./models/todoModel");
+require("dotenv").config();
 
 app.use(express.json());
+
+connectDB();
+
 app.use(logger);
 
-let todos = [
-  { id: 1, task: "Learn Node.js", completed: false },
-  { id: 2, task: "Build CRUD API", completed: true },
-];
-
 // GET All – Read
-app.get("/todos", (req, res, next) => {
+app.get("/todos", validateQuery, async (req, res, next) => {
   try {
-    //throw new Error("Simulated error for testing global error handler");
+    const filter = {};
+
+    if (req.query.completed !== undefined) {
+      filter.completed = req.query.completed;
+    }
+
+    const todos = await todoModel.find(filter);
     res.status(200).json(todos);
   } catch (err) {
     next(err);
@@ -26,23 +35,29 @@ app.get("/todos", (req, res, next) => {
 });
 
 //Get One – Read
-app.get("/todos/:id", (req, res) => {
-  const todo = todos.find((t) => t.id === parseInt(req.params.id));
-  if (!todo) return res.status(404).json({ message: "Todo not found" });
-  res.status(200).json(todo);
+app.get("/todos/:id", validateId, async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const todo = await todoModel.findById(id);
+
+    if (!todo) {
+      return res.status(404).json({ message: "No todo found" });
+    }
+    res.status(200).json(todo);
+  } catch (err) {
+    next(err);
+  }
 });
 
 // POST New – Create
-app.post("/todos", validatePost, (req, res, next) => {
-  const { task, completed } = req.body;
+app.post("/todos", validatePost, async (req, res, next) => {
+  const { task, completed = false } = req.body;
   try {
-    const newTodo = {
-      id: todos.length + 1,
+    const newTodo = await todoModel.create({
       task,
-      completed: completed !== undefined ? completed : false,
-    };
+      completed,
+    });
 
-    todos.push(newTodo);
     res.status(201).json(newTodo);
   } catch (err) {
     next(err);
@@ -50,33 +65,44 @@ app.post("/todos", validatePost, (req, res, next) => {
 });
 
 // PATCH Update – Partial
-app.patch("/todos/:id", validatePatch, (req, res) => {
-  const todo = todos.find((t) => t.id === parseInt(req.params.id)); // Array.find()
-  if (!todo) return res.status(404).json({ message: "Todo not found" });
-  Object.assign(todo, req.body); // Merge: e.g., {completed: true}
-  res.status(200).json(todo);
+app.patch("/todos/:id", validatePatch, async (req, res, next) => {
+  const { id } = req.params;
+
+  try {
+    const updatedTodo = await todoModel.findByIdAndUpdate(id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!updatedTodo) {
+      return res.status(404).json({ error: "Item not found" });
+    }
+    res.status(200).json(updatedTodo);
+  } catch (err) {
+    next(err);
+  }
 });
 
 // DELETE Remove
-app.delete("/todos/:id", (req, res) => {
+app.delete("/todos/:id", validateId, async (req, res, next) => {
   const id = parseInt(req.params.id);
-  const initialLength = todos.length;
-  todos = todos.filter((t) => t.id !== id); // Array.filter() – non-destructive
-  if (todos.length === initialLength)
-    return res.status(404).json({ error: "Not found" });
-  res.status(204).send(); // Silent success
+  try {
+    const deletedTodo = await todoModel.findByIdAndDelete(req.params.id);
+
+    if (!deletedTodo) {
+      return res.status(404).json({ error: "Item not found" });
+    }
+
+    res.status(200).json({ message: "Item deleted successfully" });
+  } catch (error) {
+    next(error);
+  }
+  ccess;
 });
 
 app.get("/todos/completed", (req, res) => {
   const completed = todos.filter((t) => t.completed);
   res.status(200).json(completed); // Custom Read!
-});
-
-app.get("/Api/todos/active", (req, res) => {
-  //Added Api to the route to avoid conflict with the /todos/:id route
-
-  const active = todos.filter((t) => !t.completed);
-  res.status(200).json(active); // Custom Read!
 });
 
 app.use(globalErrorHandler);
